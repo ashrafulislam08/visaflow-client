c79482dcf1bde126cf3b4d29feab5084d851424c
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-21 22:52:59 +0600

    Added footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/shared/Navbar";
+import Footer from "../pages/shared/Footer";
 
 const MainLayout = () => {
   return (
@@ -8,6 +9,7 @@ const MainLayout = () => {
       <Navbar />
       <Outlet />
       {/* Footer */}
+      <Footer />
     </div>
   );
 };
