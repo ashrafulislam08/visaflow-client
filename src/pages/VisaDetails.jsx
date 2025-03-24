a49e7bcd65bd150dc6dd69e039f24108be3eaeb0
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-24 22:33:48 +0600

    Added modal to apply visa

--- src/pages/VisaDetails.jsx
+++ src/pages/VisaDetails.jsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
 import useAxios from "../hooks/useAxios";
 import { useEffect, useState } from "react";
+import useAuthContext from "../hooks/useAuthContext";
+import { useForm } from "react-hook-form";
 
 const VisaDetails = () => {
   const [visa, setVisa] = useState({});
+  const { register } = useForm();
+  const { user } = useAuthContext();
   const axios = useAxios();
   const { id } = useParams();
   useEffect(() => {
@@ -33,8 +37,66 @@ const VisaDetails = () => {
         </p>
         <p className="text-lg font-semibold">Fee: ${visa.fee}</p>
         <p className="text-lg font-semibold">Validity: {visa.validity}</p>
-        <button className="btn btn-neutral">Apply for Visa</button>
+        <button
+          className="btn btn-neutral"
+          onClick={() => document.getElementById("my_modal_3").showModal()}
+        >
+          Apply for Visa
+        </button>
       </div>
+
+      {/* Modal */}
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <form className="space-y-2">
+            <div>
+              <input
+                type="text"
+                {...register("applicant_email")}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                defaultValue={user?.email}
+                disabled
+                required=""
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                {...register("first_name")}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="First Name"
+                required=""
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                {...register("last_name")}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Last Name"
+                required=""
+              />
+            </div>
+            <div>
+              <input
+                type="text"
+                {...register("fee")}
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder={`$${visa.fee}`}
+                disabled
+                required=""
+              />
+            </div>
+            <button className="btn btn-neutral">Apply</button>
+          </form>
+        </div>
+      </dialog>
     </div>
   );
 };
