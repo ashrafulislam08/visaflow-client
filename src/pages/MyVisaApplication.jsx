7a8b8b73da654d85016c031a26b5e3be82600636
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-26 12:48:21 +0600

    My visa application page almost done

--- src/pages/MyVisaApplication.jsx
+++ src/pages/MyVisaApplication.jsx
@@ -10,7 +10,54 @@ const MyVisaApplication = () => {
     axios.get(`/apply/${user.email}`).then((result) => setData(result.data));
   }, []);
   console.log(data);
-  return <div>MyVisaApplication</div>;
+  return (
+    <>
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        {data?.length > 0 ? (
+          <>
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Country</th>
+                  <th>Country Name</th>
+                  <th>Visa Type</th>
+                  <th>Processing Time</th>
+                  <th>Fee</th>
+                  <th>Validity</th>
+                  <th>Application Method</th>
+                  <th>Applied Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {data.map((visa, idx) => (
+                  <>
+                    <tr>
+                      <th>{idx + 1}</th>
+                      <td>{visa.country}</td>
+                      <td>{visa.country_name}</td>
+                      <td>{visa.visa_type}</td>
+                      <td>{visa.processing_time}</td>
+                      <td>{visa.fee}</td>
+                      <td>{visa.validity}</td>
+                      <td>{visa.application_method}</td>
+                      <td>{visa.applied_data}</td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </table>
+          </>
+        ) : (
+          <div>
+            <h2 className="text-3xl font-semibold">You don't apply for visa</h2>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default MyVisaApplication;
